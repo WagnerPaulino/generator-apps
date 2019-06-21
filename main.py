434260dc46ae97ad747d20e148a1b7d30dddfa81
Wagner Paulino <wagnersantos9778@gmail.com>
2019-06-20 22:29:51 -0300

    gerando com nome

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from core import *
 from writeDirectoryNames import *
 
 dic = dict()
-dic.update({"nameProject": "tese", "version": "2.0.1"})
+dic.update({"nameProject": "react-saga-lab", "version": "0.0.1"})
 #create_files("tese/package.json","template-reactjs/package.json",dic)
 for i in range(len(getDiretoryTemplatesNameReactjs())):
-    create_files(getDiretoryNameReactjs("teste")[i],getDiretoryTemplatesNameReactjs()[i],dic)
-    print("Criado "+getDiretoryNameReactjs("teste")[i])
+    create_files(getDiretoryNameReactjs(dic.get("nameProject"))[i],getDiretoryTemplatesNameReactjs()[i],dic)
+    print("Criado "+getDiretoryNameReactjs(dic.get("nameProject"))[i])
 #create_files("teste/.gitignore","template-reactjs/.gitignore",dic)
